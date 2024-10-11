b8c57298b6f828024c1561fd7e3f789abf6f728f
ElektroPepega <filas.suchomel@gmail.com>
2024-10-11 21:11:09 +0200

    Quartz sync: Oct 11, 2024, 9:11 PM

---
+++ content/Zdroje-Použití/Převodníky.md
@@ -0,0 +1,5 @@
+## A/D Převodník
+
+
+## D/A převodník
+
--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Pedaleq",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
